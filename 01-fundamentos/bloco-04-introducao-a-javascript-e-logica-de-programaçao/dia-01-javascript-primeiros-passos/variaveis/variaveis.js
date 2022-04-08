6848b5beaf14e11da79e6599697f57ec0074a30c
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-08 19:13:40 -0300

    Realizados os exercícios introdutórios de variáveis

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/variaveis/variaveis.js
@@ -0,0 +1,11 @@
+const myName = "Tiago";
+const birthCity = "Caxias do Sul";
+birthCity = "A melhor cidade do mundo!";
+let birthYear = 1990;
+birthYear = 2030;
+
+console.log (myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//A mensagem de erro ocorre porque uma cons não pode ser reatribuida.
