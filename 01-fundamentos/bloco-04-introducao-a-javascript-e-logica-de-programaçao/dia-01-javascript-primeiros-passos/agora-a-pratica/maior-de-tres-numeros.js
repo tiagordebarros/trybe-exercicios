f48011e12646b0c0d90fb64762c936ba8dde07f6
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-09 12:51:22 -0300

    Desenvolvido o programa que retorna o maior de três números

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/agora-a-pratica/maior-de-tres-numeros.js
@@ -0,0 +1,11 @@
+const a = 2;
+const b = 2;
+const c = 5;
+
+if (a > b && a > c) {
+    console.log("O maior número é: " + a);
+} else if (b > a && b > c) {
+    console.log("O maior número é: " + b);
+} else {
+    console.log("O maior númeor é: " + c);
+}
