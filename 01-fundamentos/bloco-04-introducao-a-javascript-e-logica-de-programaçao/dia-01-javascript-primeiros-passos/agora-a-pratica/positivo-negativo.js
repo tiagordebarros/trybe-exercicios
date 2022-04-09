ee03b2f4a5a93c8836ff52cafd067de4e3a6242c
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-09 14:02:28 -0300

    Desenvolvido o programa que verifica se o número é positivo, negativo ou zero

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/agora-a-pratica/positivo-negativo.js
@@ -0,0 +1,20 @@
+const numeroValidado = 0;
+
+if (numeroValidado > 0) {
+ console.log ("positive");
+} else if (numeroValidado < 0) {
+ console.log("negative");
+} else {
+ console.log("zero");
+}
+
+// Solução alternativa para estados booleanos
+const positivoOuNegativo = true;
+
+if ( positivoOuNegativo === true) {
+    console.log("positive");
+} else if (positivoOuNegativo === false) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
