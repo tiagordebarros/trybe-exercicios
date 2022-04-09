0a9ad89f024ae7af0b143036efb7e1247e9b4113
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-09 17:41:14 -0300

    Desenvolvido o programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/agora-a-pratica/o-melhor-de-tres-impar.js
@@ -0,0 +1,23 @@
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const terceiroNumero = 3;
+
+//101 / 2 = 50 (Resto: 1) => ÍMPAR
+
+if (primeiroNumero % 2 === 1 || segundoNumero % 2 === 1 || terceiroNumero % 2 === 1) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+//Solução alternativa
+
+// const primeiroNumero = 1;
+// const segundoNumero = 2;
+// const terceiroNumero = 3;
+
+// if (primeiroNumero % 2 !== 0 || segundoNumero % 2 !== 0 || terceiroNumero % 2 !== 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
