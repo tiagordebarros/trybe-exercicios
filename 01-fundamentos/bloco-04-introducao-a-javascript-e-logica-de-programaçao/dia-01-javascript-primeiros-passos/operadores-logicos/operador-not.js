1b24bdaa27c62bcd3c83d1803b723c10c5a24e62
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-08 22:59:08 -0300

    Realizados os exercícios introdutórios de operadores lógicos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/operadores-logicos/operador-not.js
@@ -0,0 +1,16 @@
+console.log((2 + 2) === 4);
+console.log(!(2 + 2) === 4);
+
+const squirtle = "melhor pokemon inicial";
+console.log(squirtle); //melhor pokemon inicial
+console.log(!squirtle); // false
+
+console.log(!42); // false
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+console.log(null); //null
+console.log(!null); // true
+
+console.log(undefined); //undefined
+console.log(!undefined); // true
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/operadores-logicos/operador-or.js
@@ -0,0 +1,6 @@
+let weekDay = "Domingo"
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
