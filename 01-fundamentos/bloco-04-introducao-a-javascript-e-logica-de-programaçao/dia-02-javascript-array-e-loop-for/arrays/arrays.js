fa4599e0a6215c478e3c517291d7604189babb99
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-04-10 21:07:24 -0300

    Realizados os exercícios de arrays

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-02-javascript-array-e-loop-for/arrays/arrays.js
@@ -0,0 +1,21 @@
+// Exercício 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercício 2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//Exercício 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+
+console.log(menu);
