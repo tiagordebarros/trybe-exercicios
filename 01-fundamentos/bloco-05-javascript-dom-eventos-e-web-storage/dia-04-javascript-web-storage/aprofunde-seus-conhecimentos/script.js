72e21ece3f5c13d216125ec222aa3eeab64eacb7
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-05-02 19:54:55 -0300

    Implementada a função para alterar o tamanho da fonte

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/aprofunde-seus-conhecimentos/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/aprofunde-seus-conhecimentos/script.js
@@ -16,9 +16,36 @@ function changeTextColor() {
         const textColorIcon = document.getElementById("text-color");
 
         textColorIcon.addEventListener("click", function () {
-        p[index].classList.toggle("alternative-text-color");
+            p[index].classList.toggle("alternative-text-color");
         });
     }
 };
 
 changeTextColor();
+
+//Altera o tamanho da fonte
+function changeFontSize() {
+
+    let fontSizeOptIcon = document.querySelector("#font-size-opt");
+    fontSizeOptIcon.addEventListener("change", function () {
+        const p = document.querySelectorAll("p");
+        for (let index = 0; index < p.length; index += 1) {
+            if (fontSizeOptIcon.value === "12") {
+                p[index].style.fontSize = "12px";
+            } else if (fontSizeOptIcon.value === "14") {
+                p[index].style.fontSize = "14px";
+            } else if (fontSizeOptIcon.value === "16") {
+                p[index].style.fontSize = "16px";
+            } else if (fontSizeOptIcon.value === "18") {
+                p[index].style.fontSize = "18px";
+            } else if (fontSizeOptIcon.value === "20") {
+                p[index].style.fontSize = "20px";
+            } else if (fontSizeOptIcon.value === "24") {
+                p[index].style.fontSize = "24px";
+            } else if (fontSizeOptIcon.value === "26") {
+                p[index].style.fontSize = "26px";
+            }
+        }
+    });
+};
+changeFontSize();
