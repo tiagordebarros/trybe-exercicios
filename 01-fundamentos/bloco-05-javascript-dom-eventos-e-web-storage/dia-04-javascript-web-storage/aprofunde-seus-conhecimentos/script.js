e9f0cc9275b3ca01f6631cc9177da7cdb29d6748
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-05-02 16:13:37 -0300

    Implementada a função para alterar a cor do texto

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/aprofunde-seus-conhecimentos/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/aprofunde-seus-conhecimentos/script.js
@@ -3,8 +3,22 @@ function changeBackgroundColor() {
     const body = document.querySelector("body");
     const backgroundColorIcon = document.getElementById("background-color");
 
-    backgroundColorIcon.addEventListener("click", function (event) {
+    backgroundColorIcon.addEventListener("click", function () {
         body.classList.toggle("dark-mode");
     });
 };
 changeBackgroundColor();
+
+//Altera a cor do texto
+function changeTextColor() {
+    const p = document.querySelectorAll("p");
+    for (let index = 0; index < p.length; index += 1) {
+        const textColorIcon = document.getElementById("text-color");
+
+        textColorIcon.addEventListener("click", function () {
+        p[index].classList.toggle("alternative-text-color");
+        });
+    }
+};
+
+changeTextColor();
