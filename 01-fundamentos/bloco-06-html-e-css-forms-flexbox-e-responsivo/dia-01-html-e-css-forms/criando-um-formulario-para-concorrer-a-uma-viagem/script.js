6286cfa2e41e3368a62c44b2ba347ab88a80f3a4
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-05-09 21:43:26 -0300

    Implementada a função para validar se todos os campos obrigatórios foram preenchidos

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/criando-um-formulario-para-concorrer-a-uma-viagem/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/criando-um-formulario-para-concorrer-a-uma-viagem/script.js
@@ -1,3 +1,18 @@
 //Prevent default para interromper a execução padrão do botão submeter
+// const buttonSubmit = document.getElementById("submit");
+// buttonSubmit.addEventListener("click", (event) => event.preventDefault());
+
 const buttonSubmit = document.getElementById("submit");
-buttonSubmit.addEventListener("click", (event) => event.preventDefault());
+const textArea = document.querySelector("#contestResponse");
+const fullNameField = document.querySelector("#fullName");
+const emailField = document.querySelector("#email");
+const agreementField = document.querySelector("#trpeAgreement");
+
+buttonSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (textArea.maxLength <= "500" && fullNameField.value !== "" && emailField.value !== "" && agreementField.checked === true) {
+        alert("Dados enviados com sucesso!");
+    } else {
+        alert("Dados inválidos!");
+    }
+});
