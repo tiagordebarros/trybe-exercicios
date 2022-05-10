cb7947e8e61924741565fa37c1eee83da04885c6
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-05-09 21:53:37 -0300

    Implementada a função que apaga todos os campos do formulário utilizando o método reset

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/criando-um-formulario-para-concorrer-a-uma-viagem/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/criando-um-formulario-para-concorrer-a-uma-viagem/script.js
@@ -2,6 +2,7 @@
 // const buttonSubmit = document.getElementById("submit");
 // buttonSubmit.addEventListener("click", (event) => event.preventDefault());
 
+//Utiliza o prevent default e válida se todos os dados obrigatórios foram preenchidos retornando uma mensagem
 const buttonSubmit = document.getElementById("submit");
 const textArea = document.querySelector("#contestResponse");
 const fullNameField = document.querySelector("#fullName");
@@ -11,8 +12,14 @@ const agreementField = document.querySelector("#trpeAgreement");
 buttonSubmit.addEventListener("click", (event) => {
     event.preventDefault();
     if (textArea.maxLength <= "500" && fullNameField.value !== "" && emailField.value !== "" && agreementField.checked === true) {
-        alert("Dados enviados com sucesso!");
+        alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.");
     } else {
         alert("Dados inválidos!");
     }
 });
+
+//Apaga todos os campos do formulário utilizando o método reset
+const buttonClear = document.querySelector("#clear");
+const tripForm = document.querySelector("#tripForm");
+
+buttonClear.addEventListener("click", () => tripForm.reset());
