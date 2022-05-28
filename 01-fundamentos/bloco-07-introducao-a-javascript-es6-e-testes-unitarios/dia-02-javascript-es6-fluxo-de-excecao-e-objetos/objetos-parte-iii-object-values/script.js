73bc6458cbd1e6868ffdb4b48efb3d6dddb1a309
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-05-27 21:24:12 -0300

    Realizados os exemplos de: Objetos - Parte III

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/objetos-parte-iii-object-values/script.js
@@ -0,0 +1,69 @@
+// Exemplo 1 -> Utilizando FOR...IN
+// const coolestTvShow = {
+//     name: "BoJack Horseman",
+//     genre: "adult animation",
+//     createdBy: "Raphael Bob-Waksberg",
+//     favoriteCharacter: "Princess Carolyn",
+//     quote: "Princess Carolyn always lands on her feet.",
+//     seasons: 6,
+//   };
+  
+//   for (const property in coolestTvShow) {
+//     console.log(coolestTvShow[property]);
+//   }
+  
+  // BoJack Horseman
+  // adult animation
+  // Raphael Bob-Waksberg
+  // Princess Carolyn
+  // Princess Carolyn always lands on her feet.
+  // 6
+
+// Exemplo 2 -> Utilizando Object.values
+// const coolestTvShow = {
+//     name: "BoJack Horseman",
+//     genre: "adult animation",
+//     createdBy: "Raphael Bob-Waksberg",
+//     favoriteCharacter: "Princess Carolyn",
+//     quote: "Princess Carolyn always lands on her feet.",
+//     seasons: 6,
+//   };
+
+//   console.log(Object.values(coolestTvShow));
+  
+  // [
+  //   'BoJack Horseman',
+  //   'adult animation',
+  //   'Raphael Bob-Waksberg',
+  //   'Princess Carolyn',
+  //   'Princess Carolyn always lands on her feet.'
+  //    6
+  // ]
+
+// Exemplo 3 -> Outro exemplo sem Object.values e com Object.values
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+//   const listSkillsValuesWithValues = (student) => {
+//     return Object.values(student);
+//   };
+  
+  // Sem Object.values
+  console.log(listSkillsValuesWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
