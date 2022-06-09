f0b4bc813ababfdcf2546f35bdcbd5b94f5330ac
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-09 15:28:52 -0300

    Implementada a função greet

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/agora-a-pratica/exercicio-8.js
@@ -0,0 +1,4 @@
+const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
