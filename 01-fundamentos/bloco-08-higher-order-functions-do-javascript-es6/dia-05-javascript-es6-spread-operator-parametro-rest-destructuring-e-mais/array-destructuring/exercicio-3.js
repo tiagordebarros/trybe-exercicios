a37f486953b9fa9128ad584738dace42c1170b93
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-08 22:56:26 -0300

    Realizados os exercícios de base sobre Array Destructuring

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/array-destructuring/exercicio-3.js
@@ -0,0 +1,11 @@
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+const [, , , seis, oito, dez, doze] = numerosPares;
+console.log([seis, oito, dez, doze]);
+
+// Solução alternativa
+[, , , ...numerosPares] = numerosPares;
+console.log(numerosPares);
