31d1bdb85e87a6f56c545590efa08a7dbb30aa2f
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-09 19:19:13 -0300

    Realizados os exercícios de base sobre Callbacks

---
+++ 02-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/callbacks/exercicio-2.js
@@ -0,0 +1,17 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+    setTimeout(() => {
+        const user = {
+            firstName: "Ivan",
+            lastName: "Ivanovich",
+            nationality: "Russian",
+        };
+        console.log(callback(user));
+    }, delay());
+};
+getUser(userFullName);
+getUser(userNationality);
