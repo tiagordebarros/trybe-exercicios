d4aa5b3922e29dc80450c0d53e763e85e52342b0
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-08 23:50:38 -0300

    Realizados os exercícios de base sobre Object Property Shorthand

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/object-property-shorthand/script.js
@@ -0,0 +1,13 @@
+// Função original
+// const getPosition = (latitude, longitude) => ({
+//     latitude: latitude,
+//     longitude: longitude
+// });
+// console.log(getPosition(-19.8157, -43.9542));
+
+// Utilizando Object Property Shorthand
+const getPosition = (latitude, longitude) => ({
+    latitude,
+    longitude
+});
+console.log(getPosition(-19.8157, -43.9542));
