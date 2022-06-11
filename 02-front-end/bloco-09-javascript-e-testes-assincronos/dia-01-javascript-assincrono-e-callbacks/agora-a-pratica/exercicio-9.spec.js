8084c1ca2c344bebbe4b2baa5ed909412ccd05f1
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-10 23:58:57 -0300

    Respondido a sequência de testes do exercício 10

--- 02-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/agora-a-pratica/exercicio-9.spec.js
+++ 02-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/agora-a-pratica/exercicio-9.spec.js
@@ -2,11 +2,25 @@
 const { getPokemonDetails } = require("./get-pokemon-details");
 
 describe("A função getPokemonDetails", () => {
-  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", () => {
-    // Escreva aqui seu código
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const expectedError = new Error('Não temos esse pokémon para você :(');
+
+    function callback(error, result) {
+      expect(error).toEqual(expectedError);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Pikachu', callback);
   });
 
-  it("retorna um pokemon que existe no banco de dados", () => {
-    // Escreva aqui seu código
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const expectedString = 'Olá, seu pokémon é o Bulbasaur, o tipo dele é Grass e a habilidade principal dele é Razor Leaf';
+
+    function callback(error, result) {
+        expect(result).toBe(expectedString);
+        done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Bulbasaur', callback);
   });
 });
