74ddd499223eb0a4f77117e0793cda9d93202ff0
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-06-10 23:48:10 -0300

    Implementados os testes para a função getPokemonDetails

---
+++ 02-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/agora-a-pratica/exercicio-9.spec.js
@@ -0,0 +1,12 @@
+// Verifique se a importação do arquivo correto está sendo feita.
+const { getPokemonDetails } = require("./get-pokemon-details");
+
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", () => {
+    // Escreva aqui seu código
+  });
+
+  it("retorna um pokemon que existe no banco de dados", () => {
+    // Escreva aqui seu código
+  });
+});
