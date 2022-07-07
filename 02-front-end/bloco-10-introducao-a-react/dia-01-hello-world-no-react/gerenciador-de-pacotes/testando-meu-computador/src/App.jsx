8cfb403487f350d40850a76b95f013369df5d994
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-07 13:52:44 -0300

    Criado elemento JSX e o incorporado a uma tag h1

--- 02-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/gerenciador-de-pacotes/testando-meu-computador/src/App.jsx
+++ 02-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/gerenciador-de-pacotes/testando-meu-computador/src/App.jsx
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const textJSX = "Hello, JSX";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hello, World!</code>.
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>{textJSX}</h1>
         </a>
       </header>
     </div>
