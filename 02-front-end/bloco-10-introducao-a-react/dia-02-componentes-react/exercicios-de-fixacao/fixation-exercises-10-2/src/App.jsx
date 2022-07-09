f3984d5dd388c790c43b6401fac352b7a84d3a67
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-09 20:54:19 -0300

    Realizados os exercícios de fixação da seção Composição de componentes

--- 02-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/exercicios-de-fixacao/fixation-exercises-10-2/src/App.jsx
+++ 02-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/exercicios-de-fixacao/fixation-exercises-10-2/src/App.jsx
@@ -1,15 +1,37 @@
+import React from 'react';
 import './App.css';
-import Image from './Image';
+import Order from './Order';
 
-function App() {
-  return (
-    <div className="App">
-      <Image
-      source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
-      alternativeText="Cute cat staring"
-      />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         <Order order={headphone}/>
+         <Order order={energyDrink}/>
+      </div>
+    );
+  }
 }
 
 export default App;
