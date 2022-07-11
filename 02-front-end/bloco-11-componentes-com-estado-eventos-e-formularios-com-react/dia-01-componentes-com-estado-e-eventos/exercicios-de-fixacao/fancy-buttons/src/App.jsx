cee6321bf3b75250533ef619a12c81022074053d
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-10 23:24:45 -0300

    Realizados o exercício de fixação 2 da seção Introdução aos eventos

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css';
 import { Component } from 'react';
 
-const handleClick = () => {
-  console.log('Clicou!!!');
+const handleClickThree = () => {
+  console.log('Clicou no 3!!!');
+}
+const handleClickTwo = () => {
+  console.log('Clicou no 2!!!');
+}
+
+const handleClickOne = () => {
+  console.log('Clicou no 1!!!');
 }
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <button onClick={handleClick}>Clique</button>
+        <button onClick={handleClickOne}>Clique</button>
+        <button onClick={handleClickTwo}>Clique</button>
+        <button onClick={handleClickThree}>Clique</button>
       </div>
     );
   }
