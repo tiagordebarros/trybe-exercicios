1ddfa51982bfc8b3447b1ab7e2721fcad9785b44
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-11 00:22:02 -0300

    Realizadoss os exercícios de fixação 5,6 e 7 da seção Unindo componentes com estados e eventos

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios-de-fixacao/fancy-buttons/src/App.jsx
@@ -5,18 +5,41 @@ class App extends Component {
   constructor(props){
     super(props)
     console.log(this);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClickOne = this.handleClickOne.bind(this)
+    this.handleClickTwo = this.handleClickTwo.bind(this)
+    this.handleClickThree = this.handleClickThree.bind(this)
+
+    this.state={
+      clicksCounterOne: 0,
+      clicksCounterTwo: 0,
+      clicksCounterThree: 0,
+    }
   }
 
-  handleClick(){
-    console.log(this);
-    console.log('Clicou');
-  }
+  handleClickThree(){
+    this.setState(({clicksCounterThree}, _props) => ({
+      clicksCounterThree: clicksCounterThree + 3,
+    })
+  )}
+
+  handleClickTwo(){
+    this.setState(({clicksCounterTwo}, _props) => ({
+      clicksCounterTwo: clicksCounterTwo + 2,
+    })
+  )}
+
+  handleClickOne(){
+    this.setState(({clicksCounterOne}, _props) => ({
+      clicksCounterOne: clicksCounterOne + 1,
+    })
+  )}
   render(){
     console.log(this);
     return (
       <div className="App">
-        <button onClick={this.handleClick}>Clique</button>
+        <button onClick={this.handleClickOne}>{this.state.clicksCounterOne}</button>
+        <button onClick={this.handleClickTwo}>{this.state.clicksCounterTwo}</button>
+        <button onClick={this.handleClickThree}>{this.state.clicksCounterThree}</button>
       </div>
     );
   }
--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/readme.md
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/readme.md
@@ -34,7 +34,11 @@ Sem exercícios de fixação.
 
 ### Em resumo
 
-Sem exercícios de fixação.
+8 - Defina uma lógica que estabeleça que, quando o número de cliques no botão for par, ele deve ser verde.
+
+9 - A cor atual do botão deve ser impressa num `console.log()` **de dentro** da função do evento que lida com o clique. Faça isso acontecer!
+
+🦜 **Dica:** Lembre-se de substituir a referência à função, no evento, por uma callback que invoca!
 
 ## Exercícios
 
