8b16dd6469e39251df2fbea4f71da27c8a829ce0
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-25 09:18:11 -0300

    Realizado o exercício 2 da seção 'Agora a prática(parte 1)'

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/agora-a-pratica-parte-i/npm-js-app/service.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/agora-a-pratica-parte-i/npm-js-app/service.test.js
@@ -1,9 +1,18 @@
 const service = require("./service");
 
-test("Testa se a função foi chamada, qual seu retorno e quantas vezes foi chamada.", () => {
+test('Testa se a função foi chamada, qual seu retorno e quantas vezes foi chamada.', () => {
     service.randomNumber = jest.fn().mockReturnValue(10);
 
     expect(service.randomNumber()).toBe(10);
     expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  test('Testa se a função foi chamada e a nova implementação de divisão foi aplicada.', () => {
+    service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+    
+    expect(service.randomNumber(10, 5)).toBe(2);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 5);
   });
