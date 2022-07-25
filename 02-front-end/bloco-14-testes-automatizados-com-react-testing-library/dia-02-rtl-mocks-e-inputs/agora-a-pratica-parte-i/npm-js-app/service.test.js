bb588d7b8444586f319401161fae98c1a77dc6da
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-07-25 09:38:58 -0300

    Realizado o exercício 3 da seção 'Agora a prática(parte 1)'

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/agora-a-pratica-parte-i/npm-js-app/service.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/agora-a-pratica-parte-i/npm-js-app/service.test.js
@@ -16,3 +16,25 @@ test('Testa se a função foi chamada, qual seu retorno e quantas vezes foi cham
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
     expect(service.randomNumber).toHaveBeenCalledWith(10, 5);
   });
+
+  test('Testa se a função foi chamada e qual o retorno da função.', () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+    expect(service.randomNumber(1, 2, 3)).toBe(6);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(1, 2, 3);
+  });
+
+  test('Testa se a implementação é resetada e retonra o dobro.', () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+    service.randomNumber.mockImplementation((a) => a * 2);
+    // service.randomNumber = jest.fn().mockImplementation((a) => a * 2); // Alternativamente podemos escrever desta forma
+
+    expect(service.randomNumber(4)).toBe(8);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(4);
+  });
