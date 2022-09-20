214233444440f6c7042214bf191ad41f022aef2e
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-20 20:08:32 -0300

    Realizados os exercícios de fixação da seção 'Manipulação de strings'

---
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/exercicios-de-fixacao/manipulacao-de-strings.sql
@@ -0,0 +1,16 @@
+-- Exercícios de fixação da seção "Manipulação de strings"
+
+-- Exercício 1
+SELECT UCASE('trybe');
+
+-- Exercício 2
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+
+-- Exercício 3
+SELECT CHAR_LENGTH('Uma frase qualquer');
+
+-- Exercício 4
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 12, 10);
+
+-- Exercício 5
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
