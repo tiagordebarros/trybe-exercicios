ee85df13235ab00fea2e6f5c6568a897e0011edd
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-20 21:16:01 -0300

    Realizados os exercícios de fixação da seção 'Trabalhando com datas'

---
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/exercicios-de-fixacao/trabalhando-com-datas.sql
@@ -0,0 +1,7 @@
+-- Exercícios de fixação da seção "Trabalhando com datas"
+
+-- Exercício 1
+SELECT DATEDIFF('2030-01-20', CURRENT_DATE());
+
+-- Exercício 2
+SELECT TIMEDIFF('11:00:00', '10:25:45');
