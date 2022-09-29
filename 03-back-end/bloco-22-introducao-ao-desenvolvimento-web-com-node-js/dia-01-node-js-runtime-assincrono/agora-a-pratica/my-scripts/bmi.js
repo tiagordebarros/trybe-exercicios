c272c850ab9f82f40df50d58b62aaac6ff80ba03
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-29 16:25:13 -0300

    Implementada a tabela de situação do IMC

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/agora-a-pratica/my-scripts/bmi.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/agora-a-pratica/my-scripts/bmi.js
@@ -3,8 +3,26 @@ const readline = require('readline-sync');
 const weight = readline.questionInt('What’ your weight? ');
 const height = readline.questionFloat('What’ your height? ');
 
-const bmi = () => {
-   return (weight / (height ** 2)).toFixed(2)
+const bmiStatus = (imc) => {
+    if(imc < 18.5) {
+        console.log('Abaixo do peso (magreza)');
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        console.log('Peso normal');
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        console.log('Acima do peso (sobrepeso)');
+    } else if (imc >= 30.0 && imc <= 34.9) {
+        console.log('Obesidade grau I');
+    } else if (imc >= 35.0 && imc <= 39.9) {
+        console.log('Obesidade grau II');
+    } else {
+        console.log('Obesidade graus III e IV');
+    }
 }
 
-console.log(`BMI: ${bmi()}`);
+const bmi = () => {
+    const imc = (weight / (height ** 2)).toFixed(2);
+    bmiStatus(imc);
+    return imc;
+}
+
+console.log(`IMC: ${bmi()}`);
