17992553e431fa6600481e3cf7766d350671eeec
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-03 13:15:24 -0300

    Criado um endpoint do tipo GET com a rota parametrizável

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/agora-a-pratica/basic-todo-list/src/app.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/agora-a-pratica/basic-todo-list/src/app.js
@@ -2,4 +2,30 @@ const express = require('express');
 
 const app = express();
 
+const activities = [
+    {
+        id: 1,
+        description: 'Banho no cachorro',
+        status: 'A fazer',
+    },
+    {
+        id: 2,
+        description: 'Cortar a grama',
+        status: 'A fazer',
+    },
+    {
+        id: 3,
+        description: 'Estudar JavaScript',
+        status: 'Feito',
+    },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+    const { id } = req.params;
+    
+    const findById = activities.find((activity) => activity.id === Number(id));
+    
+    return res.status(200).json(findById);
+});
+
 module.exports = app;
