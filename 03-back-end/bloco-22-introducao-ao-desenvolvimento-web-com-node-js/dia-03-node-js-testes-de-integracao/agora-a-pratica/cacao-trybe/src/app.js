bf93b39a96160b6d6739ad9425c1474d8f4b2665
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-04 10:14:30 -0300

    Inciado o servidor CacaoTrybe em Node e criado o primeiro teste e função via TDD

---
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-node-js-testes-de-integracao/agora-a-pratica/cacao-trybe/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cacaoTrybe = require('./cacaoTrybe');
+
+const app = express();
+
+app.get('/chocolates', async (req, res) => {
+    const chocolates = await cacaoTrybe.getAllChocolates();
+    res.status(200).json({ chocolates });
+});
+
+module.exports = app;
