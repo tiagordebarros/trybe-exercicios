e1a8c07f75c922c344f07ec766f4ab83ce1991fd
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-15 15:54:37 -0300

    Criado o middleware validateCreatedAt

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { validateName, validatePrice, validateDescription } = require('./middlewares');
+const {
+   validateName, validatePrice, validateDescription, validateCreatedAt,
+  } = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
- validateName, validatePrice, validateDescription,
+ validateName, validatePrice, validateDescription, validateCreatedAt,
  (_req, res) => res.status(201)
   .json({ message: 'Atividade cadastrada com sucesso!' }));
 
--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/middlewares/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/middlewares/index.js
@@ -1,9 +1,11 @@
 const validateName = require('./validateName');
 const validatePrice = require('./validatePrice');
 const validateDescription = require('./validateDescription');
+const validateCreatedAt = require('./validateCreatedAt');
 
 module.exports = {
     validateName,
     validatePrice,
     validateDescription,
+    validateCreatedAt,
 };
