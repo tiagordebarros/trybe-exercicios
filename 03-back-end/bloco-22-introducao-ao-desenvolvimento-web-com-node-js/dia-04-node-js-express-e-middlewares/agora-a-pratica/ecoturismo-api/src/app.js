9922eccf43d5a09eeaaef31f818a58aea2a4d11b
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-15 01:51:00 -0300

    Criado o endpoint POST com a rota /activities para adicionar novas atividades

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
@@ -4,4 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/activities', (_req, res) => res.status(201)
+  .json({ message: 'Atividade cadastrada com sucesso!' }));
+
 module.exports = app;
