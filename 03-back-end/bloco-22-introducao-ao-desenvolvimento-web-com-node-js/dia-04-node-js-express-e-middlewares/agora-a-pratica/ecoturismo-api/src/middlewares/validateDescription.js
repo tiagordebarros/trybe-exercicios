3af627f23f35472a160642003758b32200e79aa5
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-15 15:28:20 -0300

    Refatorado o middleware validateDescription

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/middlewares/validateDescription.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/middlewares/validateDescription.js
@@ -1,20 +1,17 @@
+const validateDescriptionMessage = (descriptionValue, res, value) => {
+  if (!descriptionValue) {
+    return res.status(400).json({ message: `O campo ${value} é obrigatório` });
+  }
+};
+
 const validateDescription = (req, res, next) => {
   const { description } = req.body;
-  const { rating, difficulty, createdAt } = description;
 
-  if (!description) {
-    return res.status(400).json({ message: 'O campo description é obrigatório' });
-  }
-  if (!rating) {
-    return res.status(400).json({ message: 'O campo rating é obrigatório' });
-  }
-  if (!difficulty) {
-    return res.status(400).json({ message: 'O campo difficulty é obrigatório' });
-  }
-  if (!createdAt) {
-    return res.status(400).json({ message: 'O campo createdAt é obrigatório' });
-  }
-  return next();
+  return validateDescriptionMessage(description, res, 'description')
+  || validateDescriptionMessage(description.rating, res, 'rating')
+  || validateDescriptionMessage(description.difficulty, res, 'difficulty')
+  || validateDescriptionMessage(description.createdAt, res, 'createdAt')
+  || next();
 };
 
 module.exports = validateDescription;
