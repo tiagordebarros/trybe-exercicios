9f3ea76a78ff24199dc400ddfbbd08e3b7acde92
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-10-15 02:03:32 -0300

    Criado o middleware validateName

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-04-node-js-express-e-middlewares/agora-a-pratica/ecoturismo-api/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { validateName } = require('./middlewares/validateName');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (_req, res) => res.status(201)
+app.post('/activities', validateName, (_req, res) => res.status(201)
   .json({ message: 'Atividade cadastrada com sucesso!' }));
 
 module.exports = app;
