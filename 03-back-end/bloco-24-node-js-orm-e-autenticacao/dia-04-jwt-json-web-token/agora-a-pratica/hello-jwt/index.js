141628b005b863f5659a4e2564a3ec47231fa3be
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-11-02 23:29:06 -0300

    Criado o endpoint /GET /users/me

--- 03-back-end/bloco-24-node-js-orm-e-autenticacao/dia-04-jwt-json-web-token/agora-a-pratica/hello-jwt/index.js
+++ 03-back-end/bloco-24-node-js-orm-e-autenticacao/dia-04-jwt-json-web-token/agora-a-pratica/hello-jwt/index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
 app.post('/login', controllers.login);
+app.get('/me', middlewares.auth, controllers.me);
 
 app.use(middlewares.error);
 
--- 03-back-end/bloco-24-node-js-orm-e-autenticacao/dia-04-jwt-json-web-token/agora-a-pratica/hello-jwt/middlewares/auth.js
+++ 03-back-end/bloco-24-node-js-orm-e-autenticacao/dia-04-jwt-json-web-token/agora-a-pratica/hello-jwt/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { token } = req.headers.authorization;
+  const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ error: { message: 'Token not found' } });
   }
