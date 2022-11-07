d59d058e5d7981126756d0ed7cd59ddac1f41855
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-11-07 19:02:03 -0300

    Desenvolvida a função add e sumArray

--- 03-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/exercicios-de-fixacao/exercises/exercises.ts
+++ 03-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/exercicios-de-fixacao/exercises/exercises.ts
@@ -5,3 +5,11 @@ export function greeter(name: string): string {
 export function personAge(name: string, age: number): string {
   return `${name} tem ${age} anos.`;
 }
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
