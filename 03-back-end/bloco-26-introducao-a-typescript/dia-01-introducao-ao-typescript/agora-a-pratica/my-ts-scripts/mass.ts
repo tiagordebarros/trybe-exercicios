0fea0f8bbbc4c4b2f5d564c5caae5ce5c77d6ad3
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-11-09 20:02:00 -0300

    Criadas as funções para chamar a função de conversão passando as entradas do usuário

--- 03-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/agora-a-pratica/my-ts-scripts/mass.ts
+++ 03-back-end/bloco-26-introducao-a-typescript/dia-01-introducao-ao-typescript/agora-a-pratica/my-ts-scripts/mass.ts
@@ -1,3 +1,5 @@
+import readlineSync from "readline-sync";
+
 const massUnits = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
 
 function convertMass(vale: number, fromUnit: string, toUnit: string): number {
@@ -7,3 +9,20 @@ function convertMass(vale: number, fromUnit: string, toUnit: string): number {
   
   return vale * Math.pow(10, exponent);
 }
+
+function exec() {
+  const value = readlineSync.questionFloat('Digite o valor a ser convertido: \n');
+  const baseUnitIndex = readlineSync.keyInSelect(massUnits, 'Escolha um número para a unidade base: ');
+  const convertUnitIndex = readlineSync.keyInSelect(massUnits, 'Escolha um número para a unidade de conversão: ');
+
+  const baseUnitChoice = massUnits[baseUnitIndex];
+  const convertUnitCHoice = massUnits[convertUnitIndex];
+
+  const result = convertMass(value, baseUnitChoice, convertUnitCHoice);
+
+  const message = `O valor ${value}${baseUnitChoice} é igual a ${result}${convertUnitCHoice}`;
+
+  return console.log(message);
+}
+
+exec();
