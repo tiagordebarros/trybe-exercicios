0f60d3c50d25165c85d9f1f26c94ec9ea06d45a0
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-11-18 11:10:12 -0300

    Criado o script que faz uso da classe Car

---
+++ 03-back-end/bloco-26-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/agora-a-pratica/index.ts
@@ -0,0 +1,25 @@
+import Car from "./classes/car";
+
+const gol = new Car("Volkswagen", "prata", 4);
+
+gol.turnOn();
+gol.speedUp();
+gol.speedDown();
+gol.turn("esquerda");
+gol.speedUp();
+gol.speedDown();
+gol.turn("direita");
+gol.speedDown();
+gol.stop();
+gol.speedUp();
+gol.speedDown();
+gol.turn("direita");
+gol.speedUp();
+gol.speedDown();
+gol.turn("esquerda");
+gol.speedUp();
+gol.speedDown();
+gol.turn("direita");
+gol.speedDown();
+gol.stop();
+gol.speedUp();
